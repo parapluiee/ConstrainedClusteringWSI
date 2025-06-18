d29cfb4f3512af0cfb49242b97caf685d911997e
parapluiee <eli.i.stafford27@gmail.com>
2025-06-18 20:20:39 +0200

    working

--- metrics.py
+++ metrics.py
@@ -6,38 +6,38 @@ def base_metrics(df, lem_most_com):
     output_dict['tot_acc'] = metrics.accuracy_score(df['gold'], df['pred'])
     output_dict['tot_f1_macro'] = metrics.f1_score(df['gold'], df['pred'], average='macro')
 
-    output_dict['per_acc'] = df.groupby('lemma').apply(lambda x: metrics.accuracy_score(x['gold'], x['pred']))
-    output_dict['per_f1_macro'] = df.groupby('lemma').apply(lambda x: metrics.f1_score(x['gold'], x['pred'], average='macro'))
+    per_acc = df.groupby('lemma').apply(lambda x: metrics.accuracy_score(x['gold'], x['pred']))
+    per_f1_macro = df.groupby('lemma').apply(lambda x: metrics.f1_score(x['gold'], x['pred'], average='macro'))
 
-    output_dict['baseline'] = df.groupby('lemma').apply(lambda x: metrics.accuracy_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
-    output_dict['beat_baseline'] = output_dict['baseline'].le(output_dict['per_acc'])
-    output_dict['beat_base_score'] = sum(output_dict['beat_baseline']) / len(output_dict['beat_baseline'])
+    baseline = df.groupby('lemma').apply(lambda x: metrics.accuracy_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
+    beat_baseline = baseline.le(per_acc)
+    output_dict['beat_base_score'] = sum(beat_baseline) / len(beat_baseline)
     return output_dict
 
 def clustering_metrics(df, lem_most_com):
     output_dict = dict()
-    output_dict['per_rand_score'] = df.groupby('lemma').apply(lambda x: metrics.rand_score(np.array(x['cluster']), np.array(x['gold'])))
-    output_dict['per_nmi(vscore)'] = df.groupby('lemma').apply(lambda x: metrics.normalized_mutual_info_score(np.array(x['cluster']), np.array(x['gold'])))
+    per_rand_score = df.groupby('lemma').apply(lambda x: metrics.rand_score(np.array(x['cluster']), np.array(x['gold'])))
+    per_nmi = df.groupby('lemma').apply(lambda x: metrics.normalized_mutual_info_score(np.array(x['cluster']), np.array(x['gold'])))
 
-    output_dict['tot_rand_score'] = output_dict['per_rand_score'].mean()
-    output_dict['tot_nmi(vscore)'] = output_dict['per_nmi(vscore)'].mean()
+    output_dict['tot_rand_score'] = per_rand_score.mean()
+    output_dict['tot_nmi(vscore)'] = per_nmi.mean()
    
 
-    output_dict['baseline_nmi(vscore)'] = df.groupby('lemma').apply(lambda x: metrics.normalized_mutual_info_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
+    baseline_nmi = df.groupby('lemma').apply(lambda x: metrics.normalized_mutual_info_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
     #bcubed requires rewriting of pandas to dictionary 
-    output_dict['per_fbcubed'] = df.groupby('lemma').apply(lambda x: bcubed_helper(x))
-    output_dict['tot_fbcubed'] = output_dict['per_fbcubed'].mean()
-    output_dict['baseline_rand'] = df.groupby('lemma').apply(lambda x: metrics.rand_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
+    per_fbcubed = df.groupby('lemma').apply(lambda x: bcubed_helper(x))
+    output_dict['tot_fbcubed'] = per_fbcubed.mean()
+    baseline_rand = df.groupby('lemma').apply(lambda x: metrics.rand_score(x['gold'], [lem_most_com[x['lemma'].iloc[0]]] * len(x)))
 
-    output_dict['beat_baseline_nmi(vscore)'] = output_dict['baseline_nmi(vscore)'].le(output_dict['per_nmi(vscore)'])
+    beat_baseline_nmi = baseline_nmi.le(per_nmi)
 
-    output_dict['baseline_fbcubed'] = df.groupby('lemma').apply(lambda x: bcubed_helper(x))
-    output_dict['beat_baseline_rand'] = output_dict['baseline_rand'].le(output_dict['per_rand_score'])
-    output_dict['beat_baseline_fbcubed'] = output_dict['baseline_fbcubed'].le(output_dict['per_fbcubed'])
-    output_dict['beat_base_score_rand'] = sum(output_dict['beat_baseline_rand']) / len(output_dict['beat_baseline_rand'])
-    output_dict['beat_base_score_nmi(vscore)'] = sum(output_dict['beat_baseline_nmi(vscore)']) / len(output_dict['beat_baseline_nmi(vscore)'])
+    baseline_fbcubed = df.groupby('lemma').apply(lambda x: bcubed_helper(x))
+    beat_baseline_rand = baseline_rand.le(per_rand_score)
+    beat_baseline_fbcubed = baseline_fbcubed.le(per_fbcubed)
+    output_dict['beat_base_score_rand'] = sum(beat_baseline_rand) / len(beat_baseline_rand)
+    output_dict['beat_base_score_nmi(vscore)'] = sum(beat_baseline_nmi) / len(beat_baseline_nmi)
     
-    output_dict['beat_base_score_fbcubed'] = sum(output_dict['beat_baseline_fbcubed']) / len(output_dict['beat_baseline_fbcubed'])
+    output_dict['beat_base_score_fbcubed'] = sum(beat_baseline_fbcubed) / len(beat_baseline_fbcubed)
     return output_dict
 
 
